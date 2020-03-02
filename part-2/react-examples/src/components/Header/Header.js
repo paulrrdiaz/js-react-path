634883fa88984b93ec0ce2185f650eac398c0542
Jose Rivas <joserivasantequera@gmail.com>
2020-03-02 18:35:16 -0500

    begin header style

--- part-2/react-examples/src/components/Header/Header.js
+++ part-2/react-examples/src/components/Header/Header.js
@@ -1,17 +1,28 @@
 import React from "react";
 import { Link } from "@reach/router";
+import { css, Global } from "@emotion/core";
+import { headerStyles } from "styles/headerStyles";
 
 export default () => {
   return (
-    <header>
-      <h1>React Demos</h1>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-        </ul>
+    <>
+      <Global
+        styles={css`
+          ${headerStyles}
+        `}
+      />
+      <nav className="menuBar">
+        <div className="logo">
+          <Link to="/">
+            Home{" "}
+            <img
+              src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1280px-React-icon.svg.png"
+              alt="logo"
+            />
+          </Link>
+        </div>
+        <div className="menuCon"></div>
       </nav>
-    </header>
+    </>
   );
 };
